b8271a6385dda1ec338d051b036e0bb11df9ac26
Luis Sandoval <fofonso_97@me.com>
2020-03-04 10:32:30 -0600

    Criba implementation in java

---
+++ haskell/practica-2/Criba.java
@@ -0,0 +1,37 @@
+// import java.util.ArrayList;
+// import java.util.List;
+
+public class Criba {
+	public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        criba(n);
+    }
+    
+    private static void criba(int n) {
+        int[] arr = new int[n];
+
+        for(int i = 2; i <= n; i++) {
+            arr[i-2] = i;
+        }
+        
+        int actual = arr[0];
+        for(int i = 0; actual * actual < n; i++) {
+            actual = arr[i];
+            if (actual != 0) {  
+                for (int j = i + 1; j < n; j++) {
+                    if (arr[j] != 0 && arr[j] % actual == 0) {
+                        arr[j] = 0;
+                    }
+                }
+            }
+        }
+
+        System.out.print("[ ");
+        for(int i = 0; i < n; i++) { 
+            if (arr[i] != 0) {
+                System.out.print(arr[i] + ", ");
+            }
+        }
+        System.out.print("]\n");
+    }
+}

51dc14dcdf9115b5465faaa0923389a684281f17
Luis Sandoval <alfsand97@gmail.com>
2020-03-12 08:13:31 -0600

    Fixed exponent sign

--- haskell/practica-1/4-raices.hs
+++ haskell/practica-1/4-raices.hs
@@ -3,8 +3,8 @@
 formula :: Bool -> Double -> Double -> Double -> Double
 
 formula signo a b c
-    | signo == True = ((-b) + sqrt ((b**2) - (4*a*c))) / (2 * a) -- X1
-    | otherwise = ((-b) - sqrt ((b**2) - (4*a*c))) / (2 * a) -- X2
+    | signo == True = ((-b) + sqrt ((b^2) - (4*a*c))) / (2 * a) -- X1
+    | otherwise = ((-b) - sqrt ((b^2) - (4*a*c))) / (2 * a) -- X2
 
 printXs x1 x2 = "X1: " ++ show x1 ++ " X2: " ++ show x2
 

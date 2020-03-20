10ba85634d200ac87c13225ee06c57662aeb8bc2
Luis Sandoval <alfsand97@gmail.com>
2020-03-20 08:00:39 -0700

    Fixed node count

--- haskell/practica-3/tree.hs
+++ haskell/practica-3/tree.hs
@@ -1,26 +1,24 @@
-data Arbol a = H a
-             | N a (Arbol a) (Arbol a)
-             deriving (Show, Eq)
+data Arbol a = H a | N a (Arbol a) (Arbol a) deriving (Show, Eq)
  
-test :: Arbol Char
-test = N '9' (N '3' (H '2') (H '4')) (H '7')
+arbolPrueba :: Arbol Char
+arbolPrueba = N 'a' (N 'b' (H 'c') (H 'd')) (H 'e')
 
-nHojas :: Arbol a -> Int
-nHojas (H _) = 1
-nHojas (N x i d) = nHojas i + nHojas d
+numHojas :: Arbol a -> Int
+numHojas (H _) = 1
+numHojas (N x y z) = numHojas y + numHojas z
 
-nNodos :: Arbol a -> Int
-nNodos (H _) = 0
-nNodos (N x i d) = 1 + nNodos i + nNodos d
+numNodos :: Arbol a -> Int
+numNodos (H _) = 1
+numNodos (N x y z) = 1 + numNodos y + numNodos z
  
 profundidad :: Arbol a -> Int
-profundidad (H _)     = 0
-profundidad (N x i d) = 1 + max (profundidad i) (profundidad d)
+profundidad (H _) = 0
+profundidad (N x y z) = 1 + max (profundidad y) (profundidad z)
  
 preorden :: Arbol a -> [a]
-preorden (H x)     = [x]
-preorden (N x i d) = x : (preorden i ++ preorden d)
+preorden (H x) = [x]
+preorden (N x y z) = x : (preorden y ++ preorden z)
 
 postorden :: Arbol a -> [a]
-postorden (H x)     = [x]
-postorden (N x i d) = postorden i ++ postorden d ++ [x]
+postorden (H x) = [x]
+postorden (N x y z) = postorden y ++ postorden z ++ [x]

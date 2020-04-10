5ed5a82b560de31d8c8a02e2a19570763ef8ddf3
Luis Sandoval <alfsand97@gmail.com>
2020-04-09 20:02:47 -0600

    Added go tutorial

---
+++ go/derek-banas/tutorial.go
@@ -0,0 +1,40 @@
+package main
+
+func main() {
+	// fmt.Println("Hello World")
+
+	// var age int = 40
+	// var favNum float64 = 1.6180339
+	// randNum := 1
+
+	// fmt.Println(age, favNum)
+	// fmt.Println(randNum)
+
+	// var numOne = 1.000
+	// var num99 = .999
+	// fmt.Println(numOne + num99)
+
+	// const pi float64 = 3.14159265
+
+	// var (
+	// 	varA = 2
+	// 	varb = 3
+	// )
+
+	// var myName string = "Derek Banas"
+
+	// fmt.Println(len(myName))
+
+	// fmt.Println(myName + " is a robot")
+
+	// var isOver40 bool = true
+
+	// fmt.Printf("%f \n", pi)
+	// fmt.Printf("%T \n", pi)
+	// fmt.Printf("%t \n", isOver40)
+	// fmt.Printf("%d \n", 100)
+	// fmt.Printf("%b \n", 100)
+	// fmt.Printf("%c \n", 100)
+	// fmt.Printf("%x \n", 100)
+	// fmt.Printf("%e \n", pi)
+}
